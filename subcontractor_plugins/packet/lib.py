09fbd5490c74cf96ba30b403abbb25f449e1da16
Peter Howe <pnhowe@gmail.com>
2021-09-01 06:59:44 -0600

    more packet work

--- subcontractor_plugins/packet/lib.py
+++ subcontractor_plugins/packet/lib.py
@@ -1,12 +1,14 @@
 import logging
 import time
 import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 import packet
 
 from subcontractor.credentials import getCredentials
 
-POLL_DELAY = 5
+POLL_DELAY = 10
 
 
 def _connect( connection_paramaters ):
@@ -21,7 +23,18 @@ def _connect( connection_paramaters ):
 
   # TODO: saninity check on creds
 
-  return packet.Manager( auth_token=creds )
+  return packet.Manager( auth_token=creds[ 'token' ] )
+
+
+def _get_vlan_map( manager, project_id ):
+  result = {}
+
+  uuid_list = manager.list_vlans( project_id )
+  for uuid in uuid_list:
+    data = manager.call_api( 'virtual-networks/{0}'.format( uuid ), type='GET' )
+    result[ data[ 'description' ] ] = uuid.id
+
+  return result
 
 
 def create( paramaters ):
@@ -32,21 +45,75 @@ def create( paramaters ):
   logging.info( 'packet: creating device "{0}"'.format( device_description ) )
   manager = _connect( connection_paramaters )
 
+  interface_map = device_paramaters[ 'interface_map' ]
+  for interface in list( interface_map.values() ):
+    interface_map[ interface[ 'physical_location' ] ] = interface
+
+  part_list = []  # we are using the same general logic as cloud-init, if these old mime finctions get removed, go see what cloud-init is doing
+
+  if 'eno2' in interface_map and interface_map[ 'eno2' ][ 'address_list' ]:
+    content = '''#cloud-config
+
+system_info:
+  network:
+    version: 2
+    ethernets:
+      eno1:
+        dhcp4: no
+        addresses: [{0}]
+        gateway4: {1}
+      eno2:
+        dhcp4: no
+        addresses: [{0}]
+        gateway4: {1}
+'''.format( interface_map[ 'eno2' ][ 'address_list' ][0][ 'address' ], interface_map[ 'eno2' ][ 'address_list' ][0][ 'gateway' ] )
+
+    part = MIMEText( content, 'cloud-config', 'ascii' )  # ascii so it won't think the utf-8 needs to base64 encoded
+    part.add_header( 'Content-Disposition', 'attachment; filename="network-config"' )
+    part_list.append( part )
+
   data = {
-           'facility': device_paramaters[ 'facility' ],
-           'plan': device_paramaters[ 'plan' ],
+           'project_id': device_paramaters[ 'project' ],
            'hostname': device_paramaters[ 'hostname' ],
-           'operating_system': device_paramaters[ 'operating_system' ]
+           'plan': device_paramaters[ 'plan' ],
+           'facility': device_paramaters[ 'facility' ],
+           'operating_system': device_paramaters[ 'operating_system' ],
+           # 'description': device_paramaters[ 'description' ]
          }
 
+  if part_list:
+    user_data = MIMEMultipart()
+    for part in part_list:
+      user_data.attach( part )
+
+    data[ 'userdata' ] = user_data.as_string()
+
   device = manager.create_device( **data )
+  device_uuid = device.id
 
-  for i in range( 0, 20 ):
-    time.sleep( POLL_DELAY )
-    device = manager.get_device( device.id )
-    logging.debug( 'packet: created device "{0}" is curently "{1}" waiting for "active", check {2} of 20...'.format( device_description, device.state, i ) )
+  logging.info( 'packet: device "{0}" created, uuid: "{1}"'.format( device_description, device_uuid ) )
 
-  logging.info( 'packet: device "{0}" created, uuid: "{1}"'.format( device_description, device.id ) )
+  # while True:
+  #   device = manager.get_device( device_uuid )
+  #   if device.state != 'queued':
+  #     break
+  #
+  #   logging.debug( 'packet: waiting for device "{0}"({1}) to start provisioning...'.format( device_description, device_uuid ) )
+  #   time.sleep( 1 )
+  #
+  # vlan_map = _get_vlan_map( manager, device_paramaters[ 'project' ] )
+  # device = manager.get_device( device_uuid )
+  # for network in device.network_ports:
+  #   iface_name = network[ 'name' ]
+  #
+  #   if iface_name in interface_map:
+  #     network_name = interface_map[ iface_name ][ 'network' ]
+  #
+  #     if network_name == 'public' or network_name not in vlan_map:
+  #       continue
+  #
+  #     manager.disbond_ports( network[ 'id' ], False )
+  #     manager.assign_port( network[ 'id' ], vlan_map[ network_name ] )
 
   return { 'done': True, 'uuid': device.id }
 
@@ -104,9 +171,9 @@ def set_power( paramaters ):
     return { 'state': curent_state }
 
   if desired_state == 'on':
-    device.action( 'power_on' )
+    device.power_on()
   elif desired_state in ( 'off', 'soft_off' ):
-    device.action( 'power_off' )
+    device.power_off()
 
   time.sleep( POLL_DELAY )  # give the device the chance to do something
 
@@ -120,7 +187,35 @@ def power_state( paramaters ):
   device_uuid = paramaters[ 'uuid' ]
   device_description = paramaters[ 'description' ]
 
-  logging.info( 'packat: getting "{0}"({1}) power state...'.format( device_description, device_uuid ) )
+  logging.info( 'packat: getting "{0}"({1}) device power state...'.format( device_description, device_uuid ) )
   manager = _connect( connection_paramaters )
   device = manager.get_device( device_uuid )
   return { 'state': _power_state_convert( device.state ) }
+
+
+def device_state( paramaters ):
+  connection_paramaters = paramaters[ 'connection' ]
+  device_uuid = paramaters[ 'uuid' ]
+  device_description = paramaters[ 'description' ]
+
+  logging.info( 'packat: getting "{0}"({1}) device state...'.format( device_description, device_uuid ) )
+  manager = _connect( connection_paramaters )
+  device = manager.get_device( device_uuid )
+  return { 'state': device.state }
+
+
+def get_interface_map( paramaters ):
+  connection_paramaters = paramaters[ 'connection' ]
+  device_uuid = paramaters[ 'uuid' ]
+  device_description = paramaters[ 'description' ]
+
+  logging.info( 'packat: getting "{0}"({1}) device ip adresses...'.format( device_description, device_uuid ) )
+  manager = _connect( connection_paramaters )
+  device = manager.get_device( device_uuid )
+
+  result = dict( ( i[ 'id' ], { 'name': i[ 'name' ], 'mac': i[ 'data' ].get( 'mac', None ), 'ip_addresses': [] } ) for i in device.network_ports )
+
+  for ip_address in device.ip_addresses:
+    result[ ip_address[ 'interface' ][ 'href' ].split( '/' )[2] ][ 'ip_addresses' ].append( { 'address': ip_address[ 'address' ], 'gateway': ip_address[ 'gateway' ], 'public': ip_address[ 'public' ] } )
+
+  return { 'interface_map': result }
